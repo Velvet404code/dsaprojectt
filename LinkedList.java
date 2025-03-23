3c32400d448bff18863f4e04f855f10320d62047
Velvet404code <164857159+Velvet404code@users.noreply.github.com>
2025-03-23 23:59:35 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,71 +1,68 @@
-import java.util.NoSuchElementException;
-
-//Node class
-class Node {
-    int data;
-    Node next;
-
-    Node(int d) {
-        this.data = d; 
-        this.next = null;
-    }
-}
-
-//LinkedList class
+// Define the LinkedList class
 class LinkedList {
-    private Node head; // Head of the list
+    Node head; // Head of the list
 
-    // Insert node at the beginning
-    public void addFirst(int newData) {
-        Node newNode = new Node(newData);
-        newNode.next = head;
-        head = newNode;
+    // Define a Node
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) { // Constructor
+            data = d;
+            next = null;
+        }
     }
 
-    // Insert node at the end
-    public void addLast(int newData) {
-        Node newNode = new Node(newData);
-        if (head == null) {
+    // Insert a node at the beginning
+    public void insertAtBeginning(int newData) {
+        Node newNode = new Node(newData); // Create a new node
+        newNode.next = head; // Link new node to the old head
+        head = newNode; // Make new node the head
+    }
+
+    // Insert a node at the end
+    public void insertAtEnd(int newData) {
+        Node newNode = new Node(newData); // Create a new node
+        if (head == null) { // If list is empty, make it head
             head = newNode;
             return;
         }
         Node temp = head;
-        while (temp.next != null) {
+        while (temp.next != null) { // Traverse to last node
             temp = temp.next;
         }
-        temp.next = newNode;
+        temp.next = newNode; // Link last node to new node
     }
 
-    // Delete node from the beginning
+    // Delete a node from the beginning
     public void deleteFromBeginning() {
-        if (head == null) {
-            throw new NoSuchElementException("List is empty");
+        if (head == null) { // If list is empty
+            System.out.println("List is empty");
+            return;
         }
-        head = head.next;
+        head = head.next; // Move head to next node
     }
 
-    // Print linked list
-    public String printList() {
-        StringBuilder result = new StringBuilder();
+    // Print the linked list
+    public void printList() {
         Node temp = head;
         while (temp != null) {
-            result.append(temp.data).append(" -> ");
+            System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
-        result.append("null");
-        return result.toString();
+        System.out.println("null");
     }
 
-    // Main method to test linked list
+    // Main method to test the linked list
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
-        list.addFirst(10);
-        list.addLast(20);
-        list.addFirst(5);
-        System.out.println(list.printList()); // Output: 5 -> 10 -> 20 -> null
+        list.insertAtBeginning(10);
+        list.insertAtEnd(20);
+        list.insertAtBeginning(5);
+        list.printList(); // Output: 5 -> 10 -> 20 -> null
 
         list.deleteFromBeginning();
-        System.out.println(list.printList()); // Output: 10 -> 20 -> null
+        list.printList(); // Output: 10 -> 20 -> null
     }
 }
